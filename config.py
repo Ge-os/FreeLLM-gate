ba691a8d5c12da7fc0e01cb3838300ee28bc56f8
Woolfer0097 <Woolfer0097@yandex.ru>
2026-02-10 23:32:46 +0300

    feat: project minimal boilerplate

---
+++ config.py
@@ -0,0 +1,32 @@
+# import pydantic
+# from pydantic.v1 import BaseSettings
+# import os
+# from dotenv import load_dotenv
+
+
+# class Settings(BaseSettings):
+#     DATABASE_PORT: int
+#     POSTGRES_PASSWORD: str
+#     POSTGRES_USER: str
+#     POSTGRES_DB: str
+#     POSTGRES_HOST: str
+#     POSTGRES_HOSTNAME: str
+
+#     JWT_PUBLIC_KEY: str
+#     JWT_PRIVATE_KEY: str
+#     REFRESH_TOKEN_EXPIRES_IN: int
+#     ACCESS_TOKEN_EXPIRES_IN: int
+#     JWT_ALGORITHM: str
+
+#     CLIENT_ORIGIN: str
+#     OPENAI_API_KEY: str
+
+#     class Config:
+#         env_file = r'.env'
+
+
+# load_dotenv()
+# API_KEY = os.environ.get("OPENAI_API_KEY")
+# CLIENT_ID = os.environ.get('client-id', None)
+# CLIENT_SECRET = os.environ.get('client-secret', None)
+# settings = Settings()
